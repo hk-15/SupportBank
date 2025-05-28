23d8e7b52438c298f4825921421de659a02f868a
hk-15 <hollyknox15@gmail.com>
2025-05-28 11:26:53 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+using (var reader = new StreamReader("./Transactions2014.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+{
+    var transactions = csv.GetRecords<Transaction>();
+    foreach (var transaction in transactions)
+    {
+        Console.WriteLine($"Date: {transaction.Date}, From: {transaction.From}, To: {transaction.To}, Narrative: {transaction.Narrative}, Amount: {transaction.Amount}");
+    }
+}
+
+public class Transaction
+{
+    public DateOnly Date { get; set; }
+    public string? From { get; set; }
+    public string? To { get; set; }
+    public string? Narrative { get; set; }
+    public float Amount { get; set; }
+}
