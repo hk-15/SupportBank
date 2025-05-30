b478257ecc6559b8544ea1a9dfbe14f6b1fd8f52
Alice Barrett <alice.barrett@softwire.com>
2025-05-30 13:40:35 +0100

    log directory

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ internal class Program
     public static void Main()
     {
         var config = new LoggingConfiguration();
-        var target = new FileTarget { FileName = @".\SupportBank\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+        string startupPath = Directory.GetCurrentDirectory();
+        var target = new FileTarget { FileName = $"{startupPath}/SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
         config.AddTarget("File Logger", target);
         config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
         LogManager.Configuration = config;
@@ -45,30 +46,28 @@ internal class Program
         var accounts = SupportBank.CreateAccounts(uniqueNames);
         Logger.Info("Sucessfully created accounts");
 
-        int initialInput;
         Logger.Info("Starting user input");
         Console.WriteLine("Do you want to list all accounts or a specific account? Press 1 to run all accounts or 2 to run a specific account.");
-        bool isInitialInputValid = int.TryParse(Console.ReadLine(), out initialInput);
-        while (!isInitialInputValid || initialInput != 1 && initialInput != 2)
+        var secondUserInput = Console.ReadLine();
+        while (secondUserInput != "1" && secondUserInput != "2")
         {
             Console.WriteLine("Please press 1 or 2 to run the program.");
-            isInitialInputValid = int.TryParse(Console.ReadLine(), out initialInput);
+            secondUserInput = Console.ReadLine();
         }
 
-        if (initialInput == 1)
+        if (secondUserInput == "1")
         {
             SupportBank.ListAll(accounts, transactions);
             Console.WriteLine("\nDo you need another service? Press 0 to return to the beginning or any other key to exit.");
-            int userChoice;
-            bool isUserChoiceValid = int.TryParse(Console.ReadLine(), out userChoice);
-            if (isUserChoiceValid && userChoice == 0)
+            var thirdUserInput = Console.ReadLine();
+            if (thirdUserInput == "0")
             {
                 Logger.Info("Restarting program . . .");
                 Program.Main();
             }
         }
 
-        if (initialInput == 2)
+        if (secondUserInput == "2")
         {
             Console.WriteLine("To view transactions, please enter a name.");
             var userNameInput = Console.ReadLine();
@@ -79,9 +78,8 @@ internal class Program
             }
             SupportBank.ListAccount(userNameInput, transactions);
             Console.WriteLine("\nDo you need another service? Press 0 to return to the beginning or any other key to exit.");
-            int userChoice;
-            bool isUserChoiceValid = int.TryParse(Console.ReadLine(), out userChoice);
-            if (isUserChoiceValid && userChoice == 0)
+            var thirdUserInput = Console.ReadLine();
+            if (thirdUserInput == "0")
             {
                 Logger.Info("Restarting program . . .");
                 Program.Main();
